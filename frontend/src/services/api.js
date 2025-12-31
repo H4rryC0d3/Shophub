be52ae07802f27ca9fb403cd5cecf3714fed15c0
Harsh Padiyar <158173756+H4rryC0d3@users.noreply.github.com>
2025-12-31 23:55:10 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL ='https://shophub-14cu.onrender.com/api';
 
 // Helper function to handle API responses
 const handleResponse = async (response) => {
@@ -231,5 +231,6 @@ const api = {
     return handleResponse(response);
   }
 };
+
 
 export default api;
